fd8ec63ce4b1f751c1aab1d29b9df9848d9f6114
Jukka Eerikäinen <jukkae@iki.fi>
2021-04-24 18:44:43 +0300

    cleanup

--- actor.rkt
+++ actor.rkt
@@ -1,7 +1,25 @@
 #lang racket
 
-(define actor<%>
-  (interface ()
-    get-next-command))
-
 (provide (all-defined-out))
+
+(require racket/serialize)
+
+(serializable-struct
+ actor
+ (name
+  [hp #:mutable]
+  max-hp
+  attack-skill
+  attack-damage
+  defense-number
+  dexterity
+  inventory
+  statuses
+  conditions
+  [current-location #:mutable]))
+
+(serializable-struct
+ pc-actor
+ (lp
+  max-lp)
+ #:super struct:actor)
--- martaanvuo.rkt
+++ martaanvuo.rkt
@@ -23,25 +23,6 @@
   day
   time-of-day))
 
-(serializable-struct
- actor
- (name
-  [hp #:mutable]
-  max-hp
-  attack-skill
-  attack-damage
-  defense-number
-  dexterity
-  inventory
-  statuses
-  conditions
-  [current-location #:mutable]))
-
-(serializable-struct
- pc-actor
- (lp
-  max-lp)
- #:super struct:actor)
 
 (define location-1
   (make-location
