78fb8ac14feb89e687c1dcb2619062ac6a54eba9
Jukka Eerikäinen <jukkae@iki.fi>
2021-05-20 00:30:29 +0300

    actual initiative system

--- martaanvuo.rkt
+++ martaanvuo.rkt
@@ -1083,11 +1083,53 @@
   (set! action-queue (remq* actions action-queue)))
 
 (define (sort-action-queue)
-  (set! action-queue (sort
-                      action-queue
-                      action-faster-than?))
   
-  )
+  (define actions-by-initiatives '())
+  (for ([action action-queue])
+    (define actor (action-actor action))
+    (define dexterity-mod (get-attribute-modifier-for (actor-dexterity actor)))
+    
+    (define action-mod 0)
+    
+    (cond ((has-tag? action 'fast)
+           (set! action-mod 2))
+          ((has-tag? action 'slow)
+           (set! action-mod -4)))
+
+
+
+    (define dice-1 (d 1 6))
+    (define dice-2 (d 1 6))
+
+    (define total (+ dice-1 dice-2 action-mod dexterity-mod))
+
+
+    (set! actions-by-initiatives (append-element actions-by-initiatives (cons total action))))
+
+  (define shuffled (shuffle actions-by-initiatives)) ; shuffle to avoid sort stability
+  (define sorted (sort shuffled
+                       (λ (a1 a2) (> (car a1) (car a2))))) ; intentionally flipped: Higher is better
+
+  (define actions
+    (for/list ([action-with-initiative sorted])
+      (define action (cdr action-with-initiative))
+      (define initiative (car action-with-initiative))
+      (define action-description
+        (string-append
+         " "
+         (actor-name (action-actor action))
+         ": "
+         (symbol->string (action-symbol action))
+         " "))
+      (list action-description (string-append " " (number->string initiative) " "))))
+  
+  (info-card actions "Action initiatives")
+
+  (set! action-queue '())
+  (for ([action-with-initiative sorted])
+    (set! action-queue (append-element action-queue (cdr action-with-initiative))))
+  
+  action-queue)
 
 (define (enqueue-npc-actions)
   (define actors (location-actors (current-location)))
@@ -1788,6 +1830,7 @@
          ; todo: define encounter levels to make it simpler
          (define i 0)
          (define enemy (make-actor "Blindscraper" 4))
+         (set-actor-dexterity! enemy 13)
          (set-trait! enemy "defense" 1)
          (set-trait! enemy "melee-attack-skill" 1)
          (set-trait! enemy "melee-attack-damage" 1)
