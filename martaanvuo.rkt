d8903b110fcc1eb1956c7e3326aa959f394e0be0
Jukka Eerikäinen <jukkae@iki.fi>
2021-05-26 20:44:20 +0300

    move some functions to round-resolver

--- martaanvuo.rkt
+++ martaanvuo.rkt
@@ -125,12 +125,7 @@
   (displayln "clean-up-dead-actor!: todo: add corpse")
   (displayln corpse))
 
-; engine / round resolver: ai dispatching
-(define (get-next-npc-action actor)
-  (case (actor-name actor)
-    (["Blindscraper"] (get-blindscraper-action actor))
-    (["Grabberkin"] (get-grabberkin-action actor))
-    (else (displayln "get-next-npc-action: unknown actor"))))
+
 
 ; engine / round resolver
 (define (get-next-action actor)
@@ -140,90 +135,6 @@
          (get-next-pc-action)))
   )
 
-; engine / round resolver: implementation detail
-(define action-queue '())
-
-; engine / round resolver
-(define (on-begin-round)
-  (set-situation-round! *situation* (add1 (situation-round *situation*)))
-  (define round-summary
-    (list
-     (list " round "
-           (string-append
-            " "
-            (number->string (situation-round *situation*))
-            " "))
-     (list " current location "
-           (string-append
-            " "
-            (symbol->string (location-type (current-location)))
-            " "))
-     (list " time of day " (string-append " " (symbol->string (time-of-day-from-jiffies (world-elapsed-time (situation-world *situation*)))) " "))
-     (list " elapsed time (total) " (string-append " " (number->string (world-elapsed-time (situation-world *situation*))) " "))
-     ))
-  (info-card round-summary (string-append "Begin round " (number->string (situation-round *situation*))))
-  
-  (set! action-queue '())
-  
-  (when (not (null? (situation-current-fragment *situation*)))
-    (current-fragment-on-begin-round!))
-  )
-
-; engine / round resolver
-(define (add-to-action-queue action)
-  (set! action-queue (cons action action-queue)))
-; engine / round resolver
-(define (remove-from-action-queue actions)
-  (set! action-queue (remq* actions action-queue)))
-; engine / round resolver
-(define (sort-action-queue)
-  
-  (define actions-by-initiatives '())
-  (for ([action action-queue])
-    (define actor (action-actor action))
-    (define dexterity-mod (get-attribute-modifier-for (actor-dexterity actor)))
-    
-    (define action-mod 0)
-    
-    (cond ((has-tag? action 'fast)
-           (set! action-mod 2))
-          ((has-tag? action 'slow)
-           (set! action-mod -4)))
-
-
-
-    (define dice-1 (d 1 6))
-    (define dice-2 (d 1 6))
-
-    (define total (+ dice-1 dice-2 action-mod dexterity-mod))
-
-
-    (set! actions-by-initiatives (append-element actions-by-initiatives (cons total action))))
-
-  (define shuffled (shuffle actions-by-initiatives)) ; shuffle to avoid sort stability
-  (define sorted (sort shuffled
-                       (λ (a1 a2) (> (car a1) (car a2))))) ; intentionally flipped: Higher is better
-
-  (define actions
-    (for/list ([action-with-initiative sorted])
-      (define action (cdr action-with-initiative))
-      (define initiative (car action-with-initiative))
-      (define action-description
-        (string-append
-         " "
-         (actor-name (action-actor action))
-         #;": "
-         #;(symbol->string (action-symbol action))
-         " "))
-      (list action-description (string-append " " (number->string initiative) " "))))
-  
-  (info-card actions "Action initiatives")
-
-  (set! action-queue '())
-  (for ([action-with-initiative sorted])
-    (set! action-queue (append-element action-queue (cdr action-with-initiative))))
-  
-  action-queue)
 
 ; engine / round resolver
 (define (enqueue-npc-actions)
--- round-resolver.rkt
+++ round-resolver.rkt
@@ -2,11 +2,18 @@
 
 (provide (all-defined-out))
 
-(require "io.rkt")
+(require "action.rkt")
+(require "actor.rkt")
+(require "blindscraper.rkt")
 (require "fragment.rkt")
 (require "fragments.rkt")
+(require "grabberkin.rkt")
+(require "io.rkt")
+(require "location.rkt")
 (require "pc.rkt")
 (require "situation.rkt")
+(require "utils.rkt")
+(require "world.rkt")
 
 ; fragment handler
 (define (current-fragment-on-begin-round!)
@@ -52,3 +59,99 @@
 (define (go-to-story-fragment id)
   (set-situation-current-fragment! *situation* (get-fragment id))
   ((story-fragment-on-enter! (situation-current-fragment *situation*))))
+
+; engine / round resolver: ai dispatching
+(define (get-next-npc-action actor)
+  (case (actor-name actor)
+    (["Blindscraper"] (get-blindscraper-action actor))
+    (["Grabberkin"] (get-grabberkin-action actor))
+    (else (displayln "get-next-npc-action: unknown actor"))))
+
+
+
+; engine / round resolver: implementation detail
+(define action-queue '())
+; engine / round resolver
+(define (add-to-action-queue action)
+  (set! action-queue (cons action action-queue)))
+; engine / round resolver
+(define (remove-from-action-queue actions)
+  (set! action-queue (remq* actions action-queue)))
+; engine / round resolver
+(define (sort-action-queue)
+  
+  (define actions-by-initiatives '())
+  (for ([action action-queue])
+    (define actor (action-actor action))
+    (define dexterity-mod (get-attribute-modifier-for (actor-dexterity actor)))
+    
+    (define action-mod 0)
+    
+    (cond ((has-tag? action 'fast)
+           (set! action-mod 2))
+          ((has-tag? action 'slow)
+           (set! action-mod -4)))
+
+
+
+    (define dice-1 (d 1 6))
+    (define dice-2 (d 1 6))
+
+    (define total (+ dice-1 dice-2 action-mod dexterity-mod))
+
+
+    (set! actions-by-initiatives (append-element actions-by-initiatives (cons total action))))
+
+  (define shuffled (shuffle actions-by-initiatives)) ; shuffle to avoid sort stability
+  (define sorted (sort shuffled
+                       (λ (a1 a2) (> (car a1) (car a2))))) ; intentionally flipped: Higher is better
+
+  (define actions
+    (for/list ([action-with-initiative sorted])
+      (define action (cdr action-with-initiative))
+      (define initiative (car action-with-initiative))
+      (define action-description
+        (string-append
+         " "
+         (actor-name (action-actor action))
+         #;": "
+         #;(symbol->string (action-symbol action))
+         " "))
+      (list action-description (string-append " " (number->string initiative) " "))))
+  
+  (info-card actions "Action initiatives")
+
+  (set! action-queue '())
+  (for ([action-with-initiative sorted])
+    (set! action-queue (append-element action-queue (cdr action-with-initiative))))
+  
+  action-queue)
+
+
+; engine / round resolver
+(define (on-begin-round)
+  (set-situation-round! *situation* (add1 (situation-round *situation*)))
+  (define round-summary
+    (list
+     (list " round "
+           (string-append
+            " "
+            (number->string (situation-round *situation*))
+            " "))
+     (list " current location "
+           (string-append
+            " "
+            (symbol->string (location-type (current-location)))
+            " "))
+     (list " time of day " (string-append " " (symbol->string (time-of-day-from-jiffies (world-elapsed-time (situation-world *situation*)))) " "))
+     (list " elapsed time (total) " (string-append " " (number->string (world-elapsed-time (situation-world *situation*))) " "))
+     ))
+  (info-card round-summary (string-append "Begin round " (number->string (situation-round *situation*))))
+  
+  (set! action-queue '())
+  
+  (when (not (null? (situation-current-fragment *situation*)))
+    (current-fragment-on-begin-round!))
+  )
+
+
