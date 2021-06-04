cb18e44edf16b408442677b410d19eb10676e506
Jukka Eerikäinen <jukkae@iki.fi>
2021-06-04 17:49:45 +0300

    start work on end-combat fragment thing

--- round-resolver.rkt
+++ round-resolver.rkt
@@ -232,7 +232,8 @@
 
   (when (and (in-combat?)
              (= (length current-enemies) 0))
-    (end-combat!))
+    (end-combat!)
+    (go-to-story-fragment 100))
   
   (when (not (null? (situation-current-fragment *situation*)))
     (current-fragment-on-end-round!)) ; TODO fragment-rounds should maybe not increase round?
