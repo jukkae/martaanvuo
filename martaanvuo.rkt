d19fe90802a3df771b7036cd14fddca5202b07f7
Jukka Eerikäinen <jukkae@iki.fi>
2021-07-30 01:23:51 +0300

    unify game-resolver to use the same procedure for continue and begin

--- martaanvuo.rkt
+++ martaanvuo.rkt
@@ -23,61 +23,35 @@
   (setup-world)
   )
 
-(define (load-save-file)
-  (newline)
-  (displayln "Loading stored progress.")
-  (define input-file (open-input-file "save.txt"))
-  (define situation (read input-file))
-  (load-situation situation)
+
+(define (resolve-game mode)
+  (case mode
+    ['begin
+     (newline)
+     (displayln "Begin a new game.")
+     (newline)
+     (displayln "Your progress is automatically stored when you make choices.")]
+    
+    ['continue
+     (newline)
+     (displayln "Loading stored progress.")
+     (define input-file (open-input-file "save.txt"))
+     (define situation (read input-file))
+     (load-situation situation)])
   
   (title)
 
-  (for ([entry (get-log)])
-    (print-paragraph (format-for-printing entry #:width 84 #:indent 4)))
-  (hr)
-
-  (let/ec win-game
-    (let continue-life ()
-      (define pc-life-end-status (resolve-life 'continue))
-      (when (eq? pc-life-end-status 'pc-dead)
-
-        (let end-of-life-menu ([verbosity 'verbose])
-          (define (handle-meta-command meta-commands-with-keys input)
-            (set! input (string-upcase input))
-            (define meta-command-with-key (hash-ref meta-commands-with-keys input '()))
-            (define meta-command (cdr meta-command-with-key))
-            (meta-command)
-            (end-of-life-menu 'verbose))
-
-          (define meta-commands (make-hash))
-          (hash-set! meta-commands "Q" (cons "[Q]: Quit." quit))
-          (hash-set! meta-commands "P" (cons "[P]: Proceed." begin-new-game))
-
-          (paragraph "Proceed?")
-          (print-meta-commands-with-keys meta-commands)
-          (define input (wait-for-input))
-          (serialize-input)
-
-          (newline)
-
-          (cond ((meta-command-valid? meta-commands input) (handle-meta-command meta-commands input))
-                (else (end-of-life-menu 'abbreviated)))))
-      (when (eq? pc-life-end-status 'win-game) (win-game))))
-  (end-game)
-  )
-
-(define (begin-new-game)
-  (newline)
-  (displayln "Begin a new game.")
-  (newline)
-  (displayln "Your progress is automatically stored whenever you make a choice.")
+  (case mode
+    ['begin (on-begin-playthrough)]
+    
+    ['continue
+     (for ([entry (get-log)])
+       (print-paragraph (format-for-printing entry #:width 84 #:indent 4)))
+     (hr)])
   
-  (title)
-  
-  (on-begin-playthrough)
   (let/ec win-game
     (let begin-new-life ()
-      (define pc-life-end-status (resolve-life 'begin))
+      (define pc-life-end-status (resolve-life mode))
       (when (eq? pc-life-end-status 'pc-dead)
 
         (let end-of-life-menu ([verbosity 'verbose])
@@ -105,12 +79,12 @@
   (end-game))
 
 ; engine / game-resolver?
-(define (begin-game)
+(define (begin-session)
   #; (random-seed 13)
 
   (if (file-exists? "save.txt")
-      (load-save-file)
-      (begin-new-game)))
+      (resolve-game 'continue)
+      (resolve-game 'begin)))
 
 ; main entrypoint
-(begin-game)
+(begin-session)
