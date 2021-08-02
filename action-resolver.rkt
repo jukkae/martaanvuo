c5c0364db97fa502de8e795f89dbd42166adb0d5
Jukka Eerikäinen <jukkae@iki.fi>
2021-08-02 11:42:13 +0300

    fixes and narration changes

--- action-resolver.rkt
+++ action-resolver.rkt
@@ -268,7 +268,7 @@
                (condition 'bleeding ; TODO: This kind of involved definition belongs to, say, conditions.rkt or something
                           ;"resolve-anklebreaker-action!: details for 'bleeding todo"
                           '() ; details
-                          (λ ()
+                          #;(λ ()
                             (define bleed-damage-roll (d 1 6)) ; could give bonus from constitution here? say, 1d6?
                             (cond ((= 1 bleed-damage-roll)
                                    (displayln "[Bleed check: 1d6 = 1: [1] => 1 dmg]")
--- locations.rkt
+++ locations.rkt
@@ -28,14 +28,22 @@
   (case n
     [(1)
      (paragraph
-   "Drawn by the magpie's call, Otava begins her ascent. The trail turns into a narrow, natural staircase of rocks, as the hillside steepens to a cliff.")]
+      "Drawn by the magpie's call, Otava begins her ascent. The trail turns into a narrow, natural staircase of rocks, as the hillside steepens to a cliff.")]
     [else
      (paragraph
-   "Otava climbs the natural stairs up to Magpie Hill.")]))
+      "Otava climbs the natural stairs up to Magpie Hill.")]))
 
+(define *magpie-hill-finish-go-to-nth* 0)
 (define (describe-magpie-hill-finish-go-to-action)
-  (paragraph "Otava comes to a slab of stone with a painting of a human figure on it. The being has the head of an ant, and its six arms are contorted in a mad dance, one pair of hands gesturing wildly with the control rods of a puppeteer. The bottom of the painting is washed away. The figure's unsettling gaze follows Otava as she goes past the painting and fills her with apprehension. It begins to drizzle.")
-  (paragraph "Soon after the dreadful painting, the rocky stairs turn back to a trail that levels out. The thick fog and the drizzle that's now a steady rain obscure much of the view. Otava is at the edge of a large plateau. The silhouette of a decaying industrial building looms in the distance. Is this it? The Facility?"))
+  (set! *magpie-hill-finish-go-to-nth* (add1 *magpie-hill-finish-go-to-nth*))
+  (define n *magpie-hill-finish-go-to-nth*)
+  (case n
+    [(1)
+     (paragraph "Otava comes to a slab of stone with a painting of a human figure on it. The being has the head of an ant, and its six arms are contorted in a mad dance, one pair of hands gesturing wildly with the control rods of a puppeteer. The bottom of the painting is washed away. The figure's unsettling gaze follows Otava as she goes past the painting and fills her with apprehension. It begins to drizzle.")
+     (paragraph "Soon after the dreadful painting, the rocky stairs turn back to a trail that levels out. The thick fog and the drizzle that's now a steady rain obscure much of the view. Otava is at the edge of a large plateau. The silhouette of a decaying industrial building looms in the distance. Is this it? The Facility?")]
+    [else
+     (paragraph
+      "The dreadful painting of Anthead God gazes at Otava mockingly as she passes the painted stone slab.")]))
   
 
 
