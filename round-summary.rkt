1b704b4343772df73a939d22e280aeb68704a320
Jukka Eerikäinen <jukkae@iki.fi>
2021-08-24 21:35:40 +0300

    cleanup

--- round-summary.rkt
+++ round-summary.rkt
@@ -10,24 +10,27 @@
       ['begin (string-append "Begin round " (number->string (current-round)))]
       ['continue (string-append "Continue round " (number->string (current-round)))]))
   
-  (define body (list
-                (list " round "
-                      (string-append
-                       " "
-                       (number->string (current-round))
-                       " "))
-                (list " current location "
-                      (string-append
-                       " "
-                       #;(get-location-name-from-location (current-location))
-                       (if (and (not (null? (current-location)))
-                                (not (void? (current-location))))
-                           (get-location-short-description (current-location))
-                           "N/A")
-                       " "))
-                (list " time of day " (string-append " " (symbol->string (time-of-day-from-jiffies (world-elapsed-time (current-world)))) " "))
-                (list " elapsed time (total) " (string-append " " (number->string (world-elapsed-time (current-world))) " "))
-                ))
-  (info-card body title)
-  (display-location-info-card (current-location) "Current location"))
+  (define body
+    (prune
+     (list
+      (list " round "
+            (string-append
+             " "
+             (number->string (current-round))
+             " "))
+      (list " current location "
+            (string-append
+             " "
+             (if (and (not (null? (current-location)))
+                      (not (void? (current-location))))
+                 (get-location-short-description (current-location))
+                 "N/A")
+             " "))
+      (list
+       " time of day "
+       (string-append " " (symbol->string (time-of-day-from-jiffies (world-elapsed-time (current-world)))) " "))
+      (list
+       " elapsed time (total) "
+       (string-append " " (number->string (world-elapsed-time (current-world))) " ")))))
+  (info-card body title))
 
