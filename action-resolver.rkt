852f39084b6822417e8ccb398e11eb533f78f83c
Jukka Eerikäinen <jukkae@iki.fi>
2021-08-25 03:30:09 +0300

    move stuff to action-resolver.rkt

--- action-resolver.rkt
+++ action-resolver.rkt
@@ -14,6 +14,7 @@
 (require "item.rkt")
 (require "pc.rkt")
 (require "state/state.rkt")
+(require "state/logging.rkt")
 (require "stance.rkt")
 (require "status.rkt")
 (require "utils.rkt")
@@ -509,18 +510,28 @@
 
 
 
-;; This should probably be formalized and eventually provided as a contract or something
-; can return:
-; 'pc-dead  when the pc is dead as a consequence of this action
-; 'ok       when the action is completely resolved and not explicitly successful or failed
-; 'success  when the action is completely resolved and explicitly successful
-; 'failed   when the action is completely resolved and fails
-; this should just dispatch, doing something else smells bad
+; what all should this return?
 (define (resolve-action! action)
   (when (actor-alive? (action-actor action))
     (case (action-symbol action)
       ; "special" actions first
+      ['end-run
+         (cond ((flag-set? 'ending-run-allowed)
+                #;(p "At least it's something.")
+                'end-run)
+               (else
+                (set-flag 'tried-to-go-back)
+                (p "The unexpected fork is worrisome. Otava must have taken the wrong turn somewhere. She decides to turn back, make sure she hasn't missed anything.")
+                (wait-for-confirm)
+                (next-chapter!) ; end chapter
+                (p "Otava is getting close to what she's looking for, but she has trouble remembering how she got here. Did she follow the trail of the Broker? Yes, yes she did. What was she doing here?")
+                (wait-for-confirm)
+                (p "The Facility. She is looking for the Facility at Martaanvuo, to pay back her debt to the Collector. Broker's trail comes to a fork.")
+                (p "To the left, the trail turns into a climb up a rocky hill. A magpie's call echoes from somewhere up the hill. An army of ants is marching down the other branch, toward what must be Martaanvuo swamp.")
+                'failure
+                ))]
       ['back-off 'ok]
+      ['win-game 'win-game]
       ['go-to-location 'ok]
       ['traverse 'ok]
       ['cancel-traverse 'ok]
--- round-resolver/pc-action-resolver.rkt
+++ round-resolver/pc-action-resolver.rkt
@@ -32,25 +32,7 @@
 
       ; pre-resolve
       (case (action-symbol action)
-        ['end-run
-         (cond ((flag-set? 'ending-run-allowed)
-                #;(p "At least it's something.")
-                (return 'end-run))
-               (else
-                (set-flag 'tried-to-go-back)
-                (p "The unexpected fork is worrisome. Otava must have taken the wrong turn somewhere. She decides to turn back, make sure she hasn't missed anything.")
-                (wait-for-confirm)
-                (next-chapter!) ; end chapter
-                (p "Otava is getting close to what she's looking for, but she has trouble remembering how she got here. Did she follow the trail of the Broker? Yes, yes she did. What was she doing here?")
-                (wait-for-confirm)
-                (p "The Facility. She is looking for the Facility at Martaanvuo, to pay back her debt to the Collector. Broker's trail comes to a fork.")
-                (p "To the left, the trail turns into a climb up a rocky hill. A magpie's call echoes from somewhere up the hill. An army of ants is marching down the other branch, toward what must be Martaanvuo swamp.")
-                (return 'failure)
-                ))]
-
-        ['win-game
-         (return 'win-game)]
-
+        
         ['traverse
          (cond ((not (pending? action))
                 (describe-begin-traverse-action action)))]
