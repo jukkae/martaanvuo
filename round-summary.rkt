f475f3db66b04b016c8281286527f4cc5f65712f
Jukka Eerikäinen <jukkae@iki.fi>
2021-08-28 10:52:42 +0300

    clean up round summary

--- round-summary.rkt
+++ round-summary.rkt
@@ -11,28 +11,35 @@
     (case mode
       ['begin (string-append "Begin round " (number->string (current-round)))]
       ['continue (string-append "Continue round " (number->string (current-round)))]))
+
+  (define time (world-elapsed-time (current-world)))
+  (define time-today (remainder time day-length))
+  (define day-number (add1 (quotient time day-length)))
   
   (define body
     (prune
      (list
-      (list " round "
-            (string-append
-             " "
+      (list (string-append
+             " round "
              (number->string (current-round))
              " "))
-      (list " current location "
-            (string-append
+      (list (string-append
+             " day "
+             (number->string day-number)
+             ", "
+             (symbol->string (time-of-day-from-jiffies (world-elapsed-time (current-world))))
+             " "
+             "("
+             (number->string time-today)
+             ")"
+             " "))
+      (list (string-append
              " "
              (if (and (not (null? (current-location)))
                       (not (void? (current-location))))
                  (get-location-short-description (current-location))
                  "N/A")
-             " "))
-      (list
-       " time of day "
-       (string-append " " (symbol->string (time-of-day-from-jiffies (world-elapsed-time (current-world)))) " "))
-      (list
-       " elapsed time (total) "
-       (string-append " " (number->string (world-elapsed-time (current-world))) " ")))))
+             " ")
+            ))))
   (info-card body title))
 
