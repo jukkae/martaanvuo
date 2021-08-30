8be53c7766b6593e3127f0f005177e6255292146
Jukka Eerikäinen <jukkae@iki.fi>
2021-08-31 00:57:56 +0300

    turn on round summaries after the build is done

--- fragments.rkt
+++ fragments.rkt
@@ -38,7 +38,8 @@
                   (wait-for-confirm)
                   (p "The trail goes past some jagged pieces of metal that stand up from the ground. This is anomaly perimeter, then – she's getting close.")
                   (p "The trail turns behind a boulder and comes to a fork. A fork? Broker had said nothing about a fork.")
-                  (p "The left branch turns into a climb up a rocky hill. A magpie's call echoes from somewhere up the hill. An army of ants is marching down the other branch, toward what must be Martaanvuo swamp."))
+                  (p "The left branch turns into a climb up a rocky hill. A magpie's call echoes from somewhere up the hill. An army of ants is marching down the other branch, toward what must be Martaanvuo swamp.")
+                  (current-show-round-summary? #t))
    #:next-fragment 'exit
    )
 
@@ -50,7 +51,7 @@
                   (wait-for-confirm)
                   (p "The trail goes past some jagged pieces of metal that stand up from the ground. This is anomaly perimeter, then – she's getting close.")
                   (p "The trail turns behind a boulder and comes to a fork. A fork? Broker had said nothing about a fork.")
-                  (p "The left branch turns into a climb up a rocky hill. A magpie's call echoes from somewhere up the hill. An army of ants is marching down the other branch, toward what must be Martaanvuo swamp."))
+                  (current-show-round-summary? #t))
    #:next-fragment 'exit)
   )
  )
