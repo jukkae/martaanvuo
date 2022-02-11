c0bc7786ab3a34ad24292486c76d3a29e7172ada
Jukka Eerikäinen <jukka.eerikainen@hatch.live>
2022-02-11 10:50:30 +0200

    refactor traverse-action resolver

--- src/resolvers/action-resolver/action-resolver.rkt
+++ src/resolvers/action-resolver/action-resolver.rkt
@@ -5,8 +5,6 @@
 (require racket/lazy-require)
 
 (require
-  "traverse-action.rkt"
-
   "../round-resolver/event.rkt"
   "../round-resolver/simulation.rkt"
   "../round-resolver/timeline.rkt"
@@ -29,6 +27,8 @@
   "../../items/item.rkt"
 
   "../../locations/0-types/location.rkt"
+  "../../locations/0-types/route.rkt"
+  "../../locations/locations.rkt"
 
   "../../pc/pc.rkt"
 
@@ -259,6 +259,64 @@
      (dev-note (format "resolve-action!: unknown action type ~a" (action-symbol action)))
      'ok
      ]))
+
+
+
+(define (resolve-cancel-traverse-action! action)
+  (reset-pending-action!)
+  (move-pc-to-location! (action-target action))
+
+  (describe-cancel-traverse-action action)
+  (display-location-info-card (current-location))
+  (when (not (null? (location-items (action-target action))))
+    (pick-up-items!))
+  'ok)
+
+
+(define (resolve-traverse-action! action)
+  (set-route-traversed! (action-target action))
+
+  (define next-location (if (memq 'a-to-b (action-details action))
+                            (route-b (action-target action))
+                            (route-a (action-target action))))
+  (move-pc-to-location! next-location)
+
+  'ok)
+
+
+(define (resolve-go-to-action! action)
+  (define elapsed-time 0)
+  (cond ((not (pending? action))
+         (describe-begin-traverse-action action)))
+
+  (define result
+    (let/ec return
+      (begin
+        ; begin advancing time
+        (define timeline
+          (advance-time-until-next-interesting-event! (action-duration action) #f)) ; no encounters
+        (set! elapsed-time (timeline-duration timeline))
+
+        #;(narrate-timeline timeline)
+
+        (when (eq? (timeline-metadata timeline) 'interrupted)
+          (return timeline))
+
+
+        (define next-location (action-target action))
+        (move-pc-to-location! next-location)
+        (location-on-enter! (current-location))
+
+        (describe-finish-traverse-action action)
+
+        (when (not (null? (location-items (action-target action))))
+          (pick-up-items!))
+
+        'ok)
+      ))
+  result)
+
+
 
 
 (define (set-pending-action! action time-left)
