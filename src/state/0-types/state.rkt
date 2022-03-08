794fec9d4063bb0c7e5d13822fd5848a96c9cc61
Jukka Eerikäinen <jukka.eerikainen@hatch.live>
2022-03-08 16:43:25 +0200

    TR struct instead of serializable-struct for state

---
+++ src/state/0-types/state.rkt
@@ -0,0 +1,41 @@
+#lang typed/racket
+
+(provide (all-defined-out))
+
+(require
+  "../../core/maybe.rkt"
+  "../../actions/action.rkt"
+  "../../actors/0-types/pc-actor.rkt"
+  "../../combat/combat-event.rkt"
+  "../../locations/0-types/location-ids.rkt"
+  "../../resolvers/round-resolver/0-types/timeline.rkt"
+  "../../tasks/task.rkt"
+  "../../world/0-types/world.rkt")
+
+(struct State
+  ([world : world]
+   [last-numeric-actor-id : Natural]
+   [log : (Listof String)]
+   [last-paragraph : String]
+   [part : Natural]
+   [chapter : Natural]
+   [prompt : String]
+   [pending-action : (Maybe action)]
+   [times-begin-traverse-narrated : (HashTable (List LocationId LocationId) Natural)]
+   [times-finish-traverse-narrated : (HashTable (List LocationId LocationId) Natural)]
+   [times-cancel-traverse-narrated : (HashTable (List LocationId LocationId) Natural)]
+   [times-species-encountered : (HashTable Symbol Natural)]
+   [flags : (Listof Symbol)]
+   [round : Natural]
+   [run : Natural]
+   [elapsed-time : Natural] ; should be in-world time
+   [in-combat? : Boolean]
+   [tasks : (Listof task)]
+   [pc : pc-actor]
+   [life : Natural]
+   [current-fragment-id : (Maybe Symbol)]
+   [combat-timeline : (Maybe (Listof combat-event))]
+   [show-round-summary? : Boolean]
+   )
+  #:prefab
+  #:mutable)
