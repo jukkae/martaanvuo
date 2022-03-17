517fafb0db651f2a2b8b06af47c021546c122393
Jukka Eerikäinen <jukkae@iki.fi>
2022-03-17 02:52:13 +0200

    writing

--- src/fragments/content/anthill.rkt
+++ src/fragments/content/anthill.rkt
@@ -10,13 +10,13 @@
            (p @~a{
  The anthill is huge. It is much taller than Otava, and it is bustling. Workers transporting raw materials, preparing, cutting, gluing, constructing. Others are tending to their crops and livestock. An orderly platoon of soldiers marches round the base. When Otava gets closer, structure emerges from the chaos of thousands of ants, and the anthill is talking to her.
 
- "BEGIN-COMMUNICATION. WHAT-IS: its type / identity?", Anthill asks.
+ "BEGIN-COMMUNICATION. WHAT-IS: its type / identity?", the anthill asks.
 
  "human / it-calls-itself Otava the Seeker.", Otava responds.
 
  "CAUSE : Otava the Seeker BRINGS 2 GRABBERKIN FINGERS
  EFFECT: 2 GRAM OF GOLD
- AGREE?", Anthill asks.
+ AGREE?", the anthill asks.
  }))
 
           #:decisions
@@ -42,6 +42,47 @@
             #:title "Ask for more."
             #:description "\"EFFECT: 3 GRAMS OF GOLD.\", Otava retorts, citing increased future risks and recent local tensions as factors increasing not only production costs on part of supplier, but value to purchaser, trying to circumvent her opponent's logic."
             #:next-fragment 'anthill-grabberkin-fingers-haggle
+            )
+           (make-decision
+            #:title "Disagree."
+            #:description "\"NO DEAL.\", Otava says and leaves."
+            )
+           ))
+
+(fragment 'anthill-1-b
+
+          (thunk
+           (p @~a{
+ The anthill waits for Otava to respond to the postulated causal chain of the cause of bringing a pair of grabberkin fingers leading to the effect of 2 grams of gold.
+ }))
+
+          #:decisions
+          (list
+           (make-decision
+            #:title "Agree."
+            #:description "\"AGREE.\", Otava agrees."
+            #:next-fragment 'anthill-grabberkin-fingers
+            )
+           (make-decision
+            #:title "Inquire about Grabberkin."
+            #:description "\"INQUIRY: grabberkin: description? DESCRIBE.\", Otava asks."
+            #:next-fragment 'anthill-grabberkin-inquiry
+            #:requirement (λ () (not (fragment-completed? 'anthill-grabberkin-inquiry)))
+            )
+           (make-decision
+            #:title "Inquire about Grabberkin fingers."
+            #:description "\"INQUIRY: grabberkin: finger: uses? DESCRIBE.\", Otava asks."
+            #:next-fragment 'anthill-grabberkin-fingers-inquiry
+            #:requirement (λ () (not (fragment-completed? 'anthill-grabberkin-fingers-inquiry)))
+            )
+           (make-decision
+            #:title "Ask for more."
+            #:description "\"EFFECT: 3 GRAMS OF GOLD.\", Otava retorts, citing increased future risks and recent local tensions as factors increasing not only production costs on part of supplier, but value to purchaser, trying to circumvent her opponent's logic."
+            #:next-fragment 'anthill-grabberkin-fingers-haggle
+            )
+           (make-decision
+            #:title "Disagree."
+            #:description "\"NO DEAL.\", Otava says and leaves."
             )
            ))
 
@@ -111,20 +152,20 @@
            (make-decision
             #:title "--"
             #:description "[Info goes here]"
-            #:next-fragment 'anthill-1
+            #:next-fragment 'anthill-1-b
             )))
 
 (fragment 'anthill-grabberkin-fingers-inquiry
           (thunk
-           (p "\"Marchgates\", Anthill begins, \"tear through space-time.\" Two fallen trees near Otava turn into huge, mangled and bent fingers, bloody and tortured and contorted, arranged in two angular, asymmetric arches leaning onto one another. There's like... she tries to... and the arches converge and there's the heavy white indoor walls of a prison-like facility, blood splattered on the walls and a pile of carnage and limbs and mutilated bodies. There's a pile of tongues of different sizes, dripping blood.")
-           (p "A glint of gold from the gnashed mouth of a crushed face. He's around 35, stubble and long dark hair. His head has been smashed in from the right, a bloody pulpy mess where there used to be a face.")
+           (p "\"Marchgates\", Anthill begins, \"tear through space-time.\" Two trees near Otava turn into huge, mangled fingers, bloody and tortured. The bones of the fingers twist and snap, as the fingers contort into two angular, asymmetric arches leaning onto one another. The arches converge, and there's the heavy white indoor walls of a prison-like facility, blood splattered on the walls and a pile of carnage and limbs and mutilated bodies. There's a pile of tongues of different sizes, dripping blood.")
+           (p "A glint of gold from the broken teeth of a gnashed mouth of a crushed face of a man. He's around 35, stubble and long dark hair. His head has been smashed in from the right, a bloody pulpy mess where there used to be the other half of his face.")
            )
           #:decisions
           (list
            (make-decision
-            #:title "Look around."
-            #:description "Marchgates tower over Otava, dripping warm blood, crushed-finger-scaffolding holding up the world. She blinks and the vision is gone and she's back at the anthill."
-            #:next-fragment 'anthill-1
+            #:title "--"
+            #:description "The marchgates tower over Otava, dripping warm blood, crushed-finger-scaffolding holding up the world. She blinks and the vision is gone and she's back at the anthill."
+            #:next-fragment 'anthill-1-b
             )))
 
 (fragment 'anthill-2
