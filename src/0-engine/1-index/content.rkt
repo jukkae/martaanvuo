b863d220e221ff8a605f74991fe9e86c8cdcad1e
Jukka Eerikäinen <jukka.eerikainen@hatch.live>
2022-03-29 18:54:41 +0300

    start splitting up pc-choices.rkt

--- src/0-engine/1-index/content.rkt
+++ src/0-engine/1-index/content.rkt
@@ -1,5 +1,7 @@
 #lang at-exp racket
 
+(require racket/lazy-require)
+
 (provide
   describe-begin-traverse-action
   describe-finish-traverse-action
@@ -8,12 +10,17 @@
   get-traverse-text
   )
 
-(require racket/lazy-require)
-
 (lazy-require ["../../1-content/narration/locations.rkt"
   (describe-begin-traverse-action
    describe-finish-traverse-action
    describe-cancel-traverse-action
    display-location-info-card
    get-traverse-text
+   )])
+
+(provide
+  get-perimeter-choices)
+
+(lazy-require ["../../1-content/world/locations/perimeter.rkt"
+  (get-perimeter-choices
    )])
