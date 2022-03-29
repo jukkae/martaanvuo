14de450284cb5bfca47c4a15f7bee368d5594646
Jukka Eerikäinen <jukka.eerikainen@hatch.live>
2022-03-29 19:49:12 +0300

    extract narrate-timeline.rkt

--- src/0-engine/5-resolvers/3-round-resolver/timeline.rkt
+++ src/0-engine/5-resolvers/3-round-resolver/timeline.rkt
@@ -11,22 +11,8 @@
   "../../3-types/event.rkt"
   "../../3-types/timeline.rkt"
 
-  "../../../1-content/narration/narrate-event.rkt"
+  "../../../1-content/narration/narrate-timeline.rkt"
   )
-
-(define (narrate-timeline timeline)
-  (define
-    displayable-events
-    (map
-     format-event-for-display
-     (timeline-events timeline)))
-  #;(info-card
-     (append
-      (tbody (tr "at" "type" "details" "interrupts action?"))
-      displayable-events)
-     (format "Timeline, duration ~a" (timeline-duration timeline)))
-  (for ([event (timeline-events timeline)])
-    (narrate-event event)))
 
 (define (process-timeline! tl)
   (for ([event (timeline-events tl)])
---
+++ src/1-content/narration/narrate-timeline.rkt
@@ -0,0 +1,27 @@
+#lang at-exp racket
+
+(provide (all-defined-out))
+
+(require
+  "narrate-event.rkt"
+
+  "../../0-engine/2-core/core.rkt"
+  "../../0-engine/2-core/io.rkt"
+
+  "../../0-engine/3-types/event.rkt"
+  "../../0-engine/3-types/timeline.rkt"
+  )
+
+(define (narrate-timeline timeline)
+  (define
+    displayable-events
+    (map
+     format-event-for-display
+     (timeline-events timeline)))
+  #;(info-card
+     (append
+      (tbody (tr "at" "type" "details" "interrupts action?"))
+      displayable-events)
+     (format "Timeline, duration ~a" (timeline-duration timeline)))
+  (for ([event (timeline-events timeline)])
+    (narrate-event event)))
