620601bf6e8f3e7d0e32291372eded66aff4251f
Jukka Eerikäinen <jukka.eerikainen@hatch.live>
2022-04-08 17:44:10 +0300

    cleanup

--- src/martaanvuo.rkt
+++ src/martaanvuo.rkt
@@ -1,8 +1,7 @@
 #lang at-exp racket
 
-
 (require "0-engine/5-resolvers/0-game-resolver/game-resolver.rkt")
-(require "1-content/fragments.rkt") ; requiring fragments has side effects and is necessary
+(require "1-content/fragments.rkt")
 
 (define (begin-session)
   (if (file-exists? "save.txt")
