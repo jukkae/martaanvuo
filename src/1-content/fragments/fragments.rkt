fed851163df3c18d6a681a9d37a940081834e6eb
Jukka Eerikäinen <jukkae@iki.fi>
2022-04-16 01:45:46 +0300

    move fragments.rkt

---
+++ src/1-content/fragments/fragments.rkt
@@ -0,0 +1,14 @@
+#lang at-exp racket
+
+(require
+  "anthill.rkt"
+  "uncategorized-fragments.rkt"
+  "luminous-precipice.rkt"
+  "magpie.rkt"
+  "martaanvuo-terminal.rkt"
+  "mieli.rkt")
+
+(provide (all-from-out
+  "../world/locations/waiting-room/fragments.rkt"))
+(require
+  "../world/locations/waiting-room/fragments.rkt")
--- src/martaanvuo.rkt
+++ src/martaanvuo.rkt
@@ -1,7 +1,7 @@
 #lang at-exp racket
 
 (require "0-engine/5-resolvers/0-game-resolver/game-resolver.rkt")
-(require "1-content/fragments.rkt")
+(require "1-content/fragments/fragments.rkt")
 
 (define (begin-session)
   (if (file-exists? "save.txt")
