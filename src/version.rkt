abc933d43f1a6d4d7ae2284e313e05d8a5eb3c49
Jukka Eerikäinen <jukkae@iki.fi>
2022-04-15 21:06:20 +0300

    bump version to 0.0.3-preview

--- src/version.rkt
+++ src/version.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (provide martaanvuo-version)
-(define martaanvuo-version "0.0.2-preview")
+(define martaanvuo-version "0.0.3-preview")
 
 ; TODO: Either this doesn't belong in this file, or this file has the wrong name
 (provide project-root-path)
