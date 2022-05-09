121ee863287d2ae4f00281c6c4d54ca65efa0666
Jukka Eerikäinen <jukkae@iki.fi>
2022-05-09 14:00:23 +0300

    bump version to mark dirty

--- src/metadata.rkt
+++ src/metadata.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (provide martaanvuo-version)
-(define martaanvuo-version "0.0.4")
+(define martaanvuo-version "0.0.4-dirty")
 
 (provide project-root-path)
 (define project-root-path (this-file))
