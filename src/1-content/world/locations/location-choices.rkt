dc3c80eae66485e402827cac6e5d52e2f0895e61
Jukka Eerikäinen <jukkae@iki.fi>
2022-05-16 02:09:05 +0300

    clean up location choices

--- src/1-content/world/locations/location-choices.rkt
+++ src/1-content/world/locations/location-choices.rkt
@@ -31,9 +31,36 @@
   "../../../0-engine/7-state/state.rkt"
   )
 
+
+; TODO: location-specific
+(define (get-magpie-hill-choices)
+  (make-choice
+    'follow-the-magpie
+    "Magpie."
+    (λ ()
+      (p "Despite the worsening rain, Otava goes into the monochrome bush.")
+      (go-to-fragment 'magpie)
+      (remove-feature-from-location! (current-location) 'magpie-effigy)
+      'end-chapter)) ; ie., 'end-round-early, plus next chapter on next round
+  )
+
+(define (get-precipice-choices)
+  (make-choice
+    'fall-down
+    "Check out the mesmerising overhang."
+    (λ ()
+      (p "The glowing fog is starting to coalesce around her, as a rock gets loose under her foot. Otava slips.")
+      (go-to-fragment 'fall-down)
+      (wait-for-confirm)
+      'end-chapter)) ; ie., 'end-round-early, plus next chapter on next round
+  ; TODO: - look into the abyss (+xp, chance of falling) - jump
+  )
+
 (define (get-location-choices location)
   (case location-id
    ['perimeter (get-perimeter-choices)]
+   ['magpie-hill (get-magpie-hill-choices)]
+   ['luminous-precipice (get-precipice-choices)]
    [else
 
     (filter
@@ -41,56 +68,6 @@
                (not (void? x))))
    (for/list ([feature (location-features (current-location))])
      (case feature
-       ['magpie-effigy
-        (make-choice
-         'follow-the-magpie
-         "Magpie."
-         (λ ()
-           (p "Despite the worsening rain, Otava goes into the monochrome bush.")
-           (go-to-fragment 'magpie)
-           (remove-feature-from-location! (current-location) 'magpie-effigy)
-           'end-chapter)) ; ie., 'end-round-early, plus next chapter on next round
-
-        ]
-
-       ['precipice
-        (make-choice
-         'fall-down
-         "Check out the mesmerising overhang."
-         (λ ()
-           (p "The glowing fog is starting to coalesce around her, as a rock gets loose under her foot. Otava slips.")
-           (go-to-fragment 'fall-down)
-           (wait-for-confirm)
-           'end-chapter)) ; ie., 'end-round-early, plus next chapter on next round
-
-        ]
-
-       ['anthill
-        (cond [(not (flag-set? 'anthill-seen))
-               (make-choice
-                'anthill
-                "Anthill."
-                (λ ()
-                  (set-flag 'anthill-seen)
-                  (go-to-fragment 'anthill-1)
-                  'end-chapter ; ie., 'end-round-early, plus next chapter on next round
-                  ))]
-              [else
-
-               ; TODO: This should be known and decided by in "content related to anthill" – for instance perhaps in fragments/anthill?
-               (define next-anthill-fragment
-                 (cond [(pc-has-item? 'grabberkin-finger)
-                        'anthill-complete-fingers]
-                       [else 'anthill-2]))
-
-               (make-choice
-                'anthill
-                "Back to Anthill."
-                (λ ()
-                  (go-to-fragment next-anthill-fragment)
-                  'end-round-early
-                  ))])
-        ]
 
        ['waiting-room-begin
         (make-choice
@@ -100,9 +77,31 @@
            (p "The penultimate step towards Ascending to a Higher Plane of Existence: To enter the waiting room!")
            (go-to-fragment 'waiting-room-1)
            'end-chapter)) ; ie., 'end-round-early, plus next chapter on next round
-
         ]
+        ['anthill
+         (cond [(not (flag-set? 'anthill-seen))
+          (make-choice
+          'anthill
+          "Anthill."
+          (λ ()
+            (set-flag 'anthill-seen)
+            (go-to-fragment 'anthill-1)
+            'end-chapter ; ie., 'end-round-early, plus next chapter on next round
+            ))]
+           [else
+            (define next-anthill-fragment
+            (cond [(pc-has-item? 'grabberkin-finger)
+                  'anthill-complete-fingers]
+                  [else 'anthill-2]))
 
-       [else (dev-note (format "unknown feature ~a" feature))])))
+            (make-choice
+            'anthill
+            "Back to Anthill."
+            (λ ()
+              (go-to-fragment next-anthill-fragment)
+              'end-round-early
+              ))])]
+
+       #;[else (dev-note (format "unknown feature ~a" feature))])))
 
     ]))
