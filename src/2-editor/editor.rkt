13b91f0b816740d976a7bb4882ac72a151c6a446
Jukka Eerikäinen <jukkae@iki.fi>
2022-06-11 17:45:57 +0300

    load selected file to editor

--- src/2-editor/editor.rkt
+++ src/2-editor/editor.rkt
@@ -7,6 +7,8 @@
 
 (define fragment-files (directory-list "1-content/fragments"))
 
+(define fragment-text (new text%))
+
 (define (get-selected-fragment-file list-box)
   (define selections (send list-box get-selections))
   (if (not (null? selections))
@@ -14,17 +16,14 @@
     '()))
 
 
-(define fragment-text (new text%))
-(send fragment-text insert "TODO")
-
-
 (define (fragment-file-selected list-box event)
   (define selected-file (get-selected-fragment-file list-box))
   (define file-name (last (explode-path selected-file)))
   (define fixed-path (build-path (bytes->path #"1-content/fragments/") file-name))
   (define loaded (open-input-file fixed-path))
-  (displayln (port->string loaded))
-  (displayln selected-file))
+  (send fragment-text delete)
+  (send fragment-text insert (port->string loaded))
+  )
 
 (editor-gui-init
  #:fragment-selector-callback fragment-file-selected)
