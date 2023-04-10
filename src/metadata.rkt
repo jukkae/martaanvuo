89c60d2b85d7c8ee089d0d7b80f81f1f5689fbfc
Jukka Eerikäinen <jukka.eerikainen@hatch.live>
2023-04-10 08:37:37 +0300

    bump version to 0.0.8

--- src/metadata.rkt
+++ src/metadata.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (provide martaanvuo-version)
-(define martaanvuo-version "0.0.7-dirty")
+(define martaanvuo-version "0.0.8")
 
 (provide project-root-path)
 (define project-root-path (this-file))
